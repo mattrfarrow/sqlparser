5a5de69cc13c70eaeeed50a5d319b8abe5978d04
Matt Farrow <matt.farrow@unrulygroup.com>
2017-11-30 17:17:06 +0000

    make multiple fields work, we're doing TDD now

--- src/main/scala/ParserUtil.scala
+++ src/main/scala/ParserUtil.scala
@@ -6,7 +6,7 @@ object ParserUtil {
   def process[T](objects: List[T], wordToString: ThingToStrings[T], sqlQuery: SqlQuery): String  = {
     val resp: List[String] =
       objects.map(o => {
-      sqlQuery.fields.map(field => wordToString.getString(field.name, o)).mkString(" | ")
+      sqlQuery.fields.map(field => wordToString.getString(field.name, o)).mkString("|")
       })
 
     resp.mkString("\n")
--- src/main/scala/SqlParser.scala
+++ src/main/scala/SqlParser.scala
@@ -9,7 +9,7 @@ case class SqlQuery(fields: Array[FieldExpr], from: String) {
   def apply(field: FieldExpr, from: String) = SqlQuery(Array(field), from)
 }
 
-case class Expression()
+class Expression()
 case class FieldExpr(name: String) extends Expression
 case class EqualsSign()
 case class Comma()
@@ -23,37 +23,32 @@ class SqlParser extends RegexParsers {
   def equalsSign: Parser[EqualsSign] = """=""".r ^^ {_ => EqualsSign()}
   def comma: Parser[Comma]   = ",".r   ^^ { _ => Comma() }
   def number: Parser[Int]    = """(0|[1-9]\d*)""".r ^^ { _.toInt }
-  def fieldorfields = fields | field
-  def fields: Parser[Array[FieldExpr]]  = field ~ comma ~ fields ^^ {case f ~ comma ~ fs => Array(f) ++ fs}
+
   def field: Parser[FieldExpr]   = """[a-z]+""".r       ^^ { s => FieldExpr(s) }
-  def expression: Parser[_ <: Expression]        = field | equalsExpression | literalStringExpr
-  def equalsExpression: Parser[EqualsExpr] =  expression ~ equalsSign ~ expression ^^ { case left ~ eq ~ right  => EqualsExpr(left, right) }
-  def literalStringExpr = "[a-z]+".r ^^ { s => LiteralStringExpr(s.drop(1).dropRight(1)) }
+  def fields: Parser[Array[FieldExpr]]  = field ~ opt(comma ~ fields) ^^ {
+    case f ~ Some(comma ~ fs) => Array(f) ++ fs
+    case f ~ None => Array(f)}
+
+  def expression: Parser[_ <: Expression] = field | equalsExpression | literalStringExpr
+
+  def equalsExpression: Parser[EqualsExpr] =
+    expression ~ equalsSign ~ expression ^^ { case left ~ eq ~ right  => EqualsExpr(left, right) }
+
+  def literalStringExpr: Parser[LiteralStringExpr] =
+    "[a-z]+".r ^^ { s => LiteralStringExpr(s.drop(1).dropRight(1)) }
+
   def select: Parser[Select] = "select".r ^^ {_ => Select()}
   def from: Parser[From] = "from".r ^^ {_ => From()}
-  def selectFrom: Parser[SqlQuery] = (select ~ fieldorfields ~ from ~ word) ^^ {case select ~ fs ~ from ~ wd => SqlQuery(fs, wd)}
+  def selectFrom: Parser[SqlQuery] = (select ~ fields ~ from ~ word) ^^ {case select ~ fs ~ from ~ wd => SqlQuery(fs, wd)}
   def word: Parser[String]   = """[a-z]+""".r       ^^ { _.toString }
 
 }
 
 object TestSqlParser extends SqlParser {
-  def main(args: Array[String]): Unit = {
-    parse(selectFrom, "select arse from cheese") match {
-      case Success(matched,_) => println(matched)
-      case Failure(msg,_) => println("FAILURE: " + msg)
-      case Error(msg,_) => println("ERROR: " + msg)
-    }
-
-    parse(selectFrom, "select arse, buckets from cheese") match {
-      case Success(matched,_) => println(matched)
-      case Failure(msg,_) => println("FAILURE: " + msg)
-      case Error(msg,_) => println("ERROR: " + msg)
-    }
-  }
 
   def parse(sql: String): Try[SqlQuery] = parse(selectFrom, sql) match {
     case Success(matched,_) => scala.util.Success(matched)
-    case Failure(msg,_) => scala.util.Failure(new Exception("Parser failed"))
+    case Failure(msg,_) => scala.util.Failure(new Exception("Parser failed: "+msg))
     case Error(msg,_) => scala.util.Failure(new Exception(msg))
   }
 }
