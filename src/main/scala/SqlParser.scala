218bd04be68a4020127c45f93fd0919b13966444
Matt Farrow <matt.farrow@unrulygroup.com>
2017-11-30 16:36:40 +0000

    working except the problem of 1 vs many fields in query

--- src/main/scala/SqlParser.scala
+++ src/main/scala/SqlParser.scala
@@ -3,20 +3,32 @@ import scala.util.parsing.combinator._
 
 
 case class Select()
-case class SqlQuery(fields: Array[Field], from: String) {
+case class SqlQuery(fields: Array[FieldExpr], from: String) {
   override def toString(): String = "SELECT " + fields.mkString(",") + " FROM " + from
+
+  def apply(field: FieldExpr, from: String) = SqlQuery(Array(field), from)
 }
-case class Field(name: String)
+
+case class Expression()
+case class FieldExpr(name: String) extends Expression
+case class EqualsSign()
 case class Comma()
 case class From()
 
+case class EqualsExpr(left: Expression, right: Expression) extends Expression
+case class LiteralStringExpr(string: String) extends Expression
+
 
 class SqlParser extends RegexParsers {
+  def equalsSign: Parser[EqualsSign] = """=""".r ^^ {_ => EqualsSign()}
   def comma: Parser[Comma]   = ",".r   ^^ { _ => Comma() }
   def number: Parser[Int]    = """(0|[1-9]\d*)""".r ^^ { _.toInt }
   def fieldorfields = fields | field
-  def field: Parser[Array[Field]]   = """[a-z]+""".r       ^^ { s => Array(Field(s)) }
-  def fields: Parser[Array[Field]]  = field ~ comma ~ field ^^ {case f ~ comma ~ fs => f ++ fs}
+  def fields: Parser[Array[FieldExpr]]  = field ~ comma ~ fields ^^ {case f ~ comma ~ fs => Array(f) ++ fs}
+  def field: Parser[FieldExpr]   = """[a-z]+""".r       ^^ { s => FieldExpr(s) }
+  def expression: Parser[_ <: Expression]        = field | equalsExpression | literalStringExpr
+  def equalsExpression: Parser[EqualsExpr] =  expression ~ equalsSign ~ expression ^^ { case left ~ eq ~ right  => EqualsExpr(left, right) }
+  def literalStringExpr = "[a-z]+".r ^^ { s => LiteralStringExpr(s.drop(1).dropRight(1)) }
   def select: Parser[Select] = "select".r ^^ {_ => Select()}
   def from: Parser[From] = "from".r ^^ {_ => From()}
   def selectFrom: Parser[SqlQuery] = (select ~ fieldorfields ~ from ~ word) ^^ {case select ~ fs ~ from ~ wd => SqlQuery(fs, wd)}
