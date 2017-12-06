ab5639398d26c06397fd605f31c05962bfb890a0
Matt Farrow <matt.farrow@unrulygroup.com>
2017-12-06 17:07:26 +0000

    Simplify the test a loooot

--- src/test/scala/com/mrfarrow/sqls/TestSqls.scala
+++ src/test/scala/com/mrfarrow/sqls/TestSqls.scala
@@ -2,25 +2,13 @@ package com.mrfarrow.sqls
 
 import java.io.File
 
-import com.mrfarrow.sqlparser.{ParserUtil, SqlParser, SqlQuery}
 import org.scalatest.{FlatSpec, Matchers}
-
-import scala.util.{Failure, Success}
-
 
 class TestSqls extends FlatSpec with Matchers {
 
-  it should "list files " in {
-    val resourcesDirectory = new File("src/test/resources")
-
-    val sql = """select name where name like 'sr*' """
-    val query = new SqlParser(new FileToStrings).parse(sql) match {
-      case t: Success[SqlQuery] => t.value
-      case t: Failure[_] => throw t.exception
-    }
-
-    val currentDir = new File(System.getProperty("user.dir"))
-    Sqls.runLs(currentDir, sql) should be ("src")
+  it should "list files" in {
+    val sql = """select name where name like 'short*'"""
+    Sqls.runLs(new File("src/test/resources"), sql) should be ("shortFile.txt")
   }
 
 }
